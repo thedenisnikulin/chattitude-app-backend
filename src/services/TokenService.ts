258a5acb49aefd126aec72af74eca36ceeafab60
thedenisnikulin <thedenisnikulin@gmail.com>
2020-11-22 19:02:13 +0300

    Code style

--- src/services/TokenService.ts
+++ src/services/TokenService.ts
@@ -9,23 +9,22 @@ export default class Token {
         if (!header) {
             res.json({data: {tokenVerificationData:{ access: false, message: 'No token provided' }}});
             return;
-        } else {
-            const token = header.split(' ')[1];
-            // console.log('tokenService token: ' + token);
-            jwt.verify(token, ACCESS_TOKEN_SECRET, (err, decodedFromToken) => {
-                if (err) {
-                    res.json({data: {tokenVerificationData: { access: false, message: 'Failed to verify token' }}});
-                    return;
-                } else {
-                    // there's decodedFromToken.user that can only be reached with casting
-                    // that's why it is wrapped in <{user: object}>
-                    const decoded = <{user: object}>decodedFromToken;
-                    const decodedUser = <ISafeUser>decoded.user;
-                    // res.json({tokenVerificationData: { access: true, user: decodedUser } });
-                    req.verifiedUser = decodedUser;
-                    next();
-                } 
-            });
         }
+		const token = header.split(' ')[1];
+		// console.log('tokenService token: ' + token);
+		jwt.verify(token, ACCESS_TOKEN_SECRET, (err, decodedFromToken) => {
+			if (err) {
+				res.json({data: {tokenVerificationData: { access: false, message: 'Failed to verify token' }}});
+				return;
+			} else {
+				// there's decodedFromToken.user that can only be reached with casting
+				// that's why it is wrapped in <{user: object}>
+				const decoded = <{user: object}>decodedFromToken;
+				const decodedUser = <ISafeUser>decoded.user;
+				// res.json({tokenVerificationData: { access: true, user: decodedUser } });
+				req.verifiedUser = decodedUser;
+				next();
+			} 
+		});
     }
 }
